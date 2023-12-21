8cdfcc4a8bbc1b5f447602017babe70355e5580d
iamrazo <bestwishtous@gmail.com>
2023-12-21 13:40:52 +0800

    feature: 环境变量变化

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 const lodash = require('lodash');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT_MAIN;
 const tokens = (process.env.TOKENS || '').split(' ').map((token) => token);
 const tokensMap = new Map();
 const maxRetries = 1;
