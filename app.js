d873d1639eaed6efa9023bc3784bfcd8a2fd1c5a
iamrazo <bestwishtous@gmail.com>
2023-12-24 04:21:26 +0800

    feature: 先不要验证

--- app.js
+++ app.js
@@ -237,14 +237,14 @@ app.post('/recognize', upload.single('file'), async (req, res) => {
 	const file = req.file;
 	const { prompt } = req.body;
 	try {
-		let apiKey = '';
 		const { GOOGLE_GEMININ_API_KEY } = process.env;
-		const authorization = lodash.get(req, 'headers.authorization');
-		if (authorization === `Bearer ${process.env.ACCESS_CODE}`) {
-			apiKey = GOOGLE_GEMININ_API_KEY;
-		} else {
-			apiKey = authorization.replace('Bearer ', '');
-		}
+		let apiKey = GOOGLE_GEMININ_API_KEY;
+		// const authorization = lodash.get(req, 'headers.authorization');
+		// if (authorization === `Bearer ${process.env.ACCESS_CODE}`) {
+		// 	apiKey = GOOGLE_GEMININ_API_KEY;
+		// } else {
+		// 	apiKey = authorization.replace('Bearer ', '');
+		// }
 		const model = new GoogleGenerativeAI(apiKey).getGenerativeModel({
 			model: 'gemini-pro-vision',
 		});
