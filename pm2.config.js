5ae9b04e617be1b0e2ed7147d924e4d4f5bbe5ce
Razo <bestwishtous@gmail.com>
2024-04-23 06:17:54 +0800

    fix: kimi先屏蔽

--- pm2.config.js
+++ pm2.config.js
@@ -31,11 +31,11 @@ module.exports = {
 			exec_mode: 'fork',
 			script: 'src/coze/app.mjs',
 		},
-		{
-			...config,
-			name: 'kimi',
-			exec_mode: 'fork',
-			script: '/kimi/dist/index.js',
-		},
+		// {
+		// 	...config,
+		// 	name: 'kimi',
+		// 	exec_mode: 'fork',
+		// 	script: '/kimi/dist/index.js',
+		// },
 	],
 };
