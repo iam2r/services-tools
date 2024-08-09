6033f32b1f126d50f244bb2296fd81c520a48402
My Name <my@example.com>
2024-08-09 09:50:01 +0800

    feat: remove ipv6

--- app.mjs
+++ app.mjs
@@ -174,7 +174,7 @@ app.get('/cf/addressesapi', async (req, res) => {
 						const [name, area] =
 							decodeURI((hash || '').replace(/^\#/, '')).match(
 								type === 'custom'
-									? /(移动|联通|电信|狮城|新加坡|香港|台湾|日本|韩国|美国|英国|法国|荷兰|波兰|芬兰|德国|都柏林|瑞典|西班牙|加拿大|澳洲|IPV6|US|DE|NL|KR|SG|AU|HK|JP|TW|DE|GB|SE|ES|CA|HKG|TOKYO|SINGAPORE|TAIPEI|PL)/i
+									? /(移动|联通|电信|狮城|新加坡|香港|台湾|日本|韩国|美国|英国|法国|荷兰|波兰|芬兰|德国|都柏林|瑞典|西班牙|加拿大|澳洲|US|DE|NL|KR|SG|AU|HK|JP|TW|DE|GB|SE|ES|CA|HKG|TOKYO|SINGAPORE|TAIPEI|PL)/i
 									: /.*/
 							) || [];
 						const formattedString =
