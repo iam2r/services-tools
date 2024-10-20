e7cbefc177020786566ac5d56ae82fc24ea31205
xxx <a@b.c>
2024-10-20 13:31:59 +0800

    feature: pure

--- app.mjs
+++ app.mjs
@@ -1,8 +1,6 @@
 import './config.js';
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import multer from 'multer';
 import cors from 'cors';
 import lodash from 'lodash';
 import axios from 'axios';
@@ -10,8 +8,6 @@ import url from 'url';
 import cron from 'node-cron';
 
 const app = express();
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 const port = process.env.PORT_MAIN;
 
 const createOpenAIHandle =
@@ -41,48 +37,11 @@ const createBaseAuthorizationHandler = (token) => (req) => {
 	}
 };
 
-const createBaseBodyModified =
-	(callback = (originalBody) => originalBody) =>
-	(proxyReq, req) => {
-		const originalBody = req.body;
-		const modifiedParamsString = JSON.stringify(
-			callback({
-				...originalBody,
-			})
-		);
-		proxyReq.setHeader('Content-Type', 'application/json');
-		proxyReq.setHeader('Content-Length', Buffer.byteLength(modifiedParamsString));
-		proxyReq.write(modifiedParamsString);
-		proxyReq.end();
-	};
-
-const createKimiOptions = (useSearch = false) => {
-	return {
-		apiKey: process.env.KIMI_TOKEN,
-		onProxyReq: createBaseBodyModified((originalBody) => {
-			return {
-				...originalBody,
-				use_search: useSearch,
-			};
-		}),
-	};
-};
-
 [
 	{
 		prefix: 'cohere',
 		target: 'https://cohere2openai.iamrazo.workers.dev',
 		apiKey: process.env.COHERE_TOKEN,
-	},
-	{
-		prefix: 'kimi',
-		target: 'https://kimi-free-api-o85q.onrender.com',
-		...createKimiOptions(),
-	},
-	{
-		prefix: 'kimi-search',
-		target: 'https://kimi-free-api-o85q.onrender.com',
-		...createKimiOptions(true),
 	},
 ].forEach(({ prefix, target, authorizationHandler, onProxyReq, apiKey = '' }) => {
 	const options = lodash.merge(
@@ -100,10 +59,6 @@ const createKimiOptions = (useSearch = false) => {
 		authorizationHandler ? { authorizationHandler } : {}
 	);
 	app.use(`/${prefix}`, createOpenAIHandle(options));
-});
-
-app.get('/healthcheck', (req, res) => {
-	res.status(200).json({ status: 'OK' });
 });
 
 app.get('/cf/get_optimization_ip', (req, res) => {
@@ -246,43 +201,9 @@ app.get('/cf/addressesapi', async (req, res) => {
 	}
 });
 
-async function fileToGenerativePart(file) {
-	return {
-		inlineData: {
-			data: file.buffer.toString('base64'),
-			mimeType: file.mimetype,
-		},
-	};
-}
-
-app.post('/recognize', upload.single('file'), async (req, res) => {
-	const file = req.file;
-	const { prompt } = req.body;
-	try {
-		const { GOOGLE_GEMININ_API_KEY } = process.env;
-		let apiKey = '';
-		const authorization = lodash.get(req, 'headers.authorization');
-		if (authorization === `Bearer ${process.env.ACCESS_CODE}`) {
-			apiKey = GOOGLE_GEMININ_API_KEY;
-		} else {
-			apiKey = authorization.replace('Bearer ', '');
-		}
-		const model = new GoogleGenerativeAI(apiKey).getGenerativeModel({
-			model: 'gemini-pro-vision',
-		});
-		const imageParts = await Promise.all([fileToGenerativePart(file)]);
-		const result = await model.generateContent([prompt || '请描述并提取一下图片内容', ...imageParts]);
-		const response = result.response;
-		const text = response.text();
-		res.status(200).json({
-			text,
-		});
-	} catch (error) {
-		console.error('Error generateContent:', error);
-		res.status(500).json({ error: 'Error generateContent' });
-	}
+app.get('/healthcheck', (req, res) => {
+	res.status(200).json({ status: 'OK' });
 });
-
 app.listen(port, () => {
 	console.log('📝 Author: Razo');
 	console.log('🌍 GitHub Repository: https://github.com/iam2r/openai-tools');
