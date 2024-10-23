1ffaa2fa80f12a7d1da10e9afb917197a0a36b26
xxx <a@b.c>
2024-10-24 01:01:05 +0800

    feature: ping every hour

--- app.mjs
+++ app.mjs
@@ -217,14 +217,22 @@ const keepAlive = () => {
 	const urls = (process.env.KEEP_ALIVE_URLS || '').split(',');
 	if (urls.length) {
 		let job = null;
-		// 每天重新生成随机时间并创建新的 cron 任务
+
+		// 每小时重新生成随机时间并创建新的 cron 任务
 		const scheduleJob = () => {
 			if (job) {
 				job.stop(); // 停止上一次的 cron 任务
 			}
-			const randomHour = Math.floor(Math.random() * 24);
-			const randomMinute = Math.floor(Math.random() * 60);
-			const cronString = `0 ${randomMinute} ${randomHour} * * *`;
+
+			const now = new Date();
+			const currentMinute = now.getMinutes();
+			let randomMinute = Math.floor(Math.random() * 60);
+
+			// 如果生成的随机分钟数已经过去，则将其设置为下一分钟
+			if (randomMinute <= currentMinute) {
+				randomMinute = (currentMinute + 1) % 60;
+			}
+			const cronString = `${randomMinute} * * * *`; // 每小时的随机分钟执行
 			console.log(`${process.env.KEEP_ALIVE_URLS} is keepalive with ${cronString}!`);
 			job = cron.schedule(cronString, () => {
 				Promise.all(urls.map((url) => fetch(url)))
@@ -234,8 +242,8 @@ const keepAlive = () => {
 
 			job.start();
 
-			// 每天重新创建任务
-			setTimeout(scheduleJob, 24 * 60 * 60 * 1000); // 24 小时后重新创建任务
+			// 每小时重新创建任务
+			setTimeout(scheduleJob, 60 * 60 * 1000); // 1 小时后重新创建任务
 		};
 
 		scheduleJob();
