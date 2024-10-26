be1f5c8c2972c44ca488b065d39ae9260abfa7d0
xxx <a@b.c>
2024-10-26 21:47:07 +0800

    feature: runtime

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,12 @@
 import 'config.js';
-import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
 import { prettyJSON } from 'hono/pretty-json';
 import router from '@/routes/index.ts';
-import { scheduleTasks } from '@/utils/schedule-tasks.ts';
-import { startLog } from '@/utils/start-log.ts';
 
-const app = new Hono();
-app
+export const app = new Hono()
 	.use(logger())
 	.use(prettyJSON())
 	.use(cors())
@@ -19,12 +15,3 @@ app
 		return c.json({ status: 'pong' });
 	})
 	.route('/', router);
-
-const port = 3000;
-
-serve({
-	fetch: app.fetch,
-	port,
-});
-startLog(port);
-scheduleTasks();
