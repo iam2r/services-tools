ca25ffa6703f546fab18bd0fa3dc3ba0886f1d3e
xxx <a@b.c>
2024-10-26 17:40:55 +0800

    fix: 修复圆角1

--- src/routes/api/sharps.ts
+++ src/routes/api/sharps.ts
@@ -12,7 +12,7 @@ sharps.get('/macos-icon', async (c) => {
 	const resultSize = Number(size);
 	const transparentMargin = Math.floor(resultSize / 6);
 	const cornerRadius = transparentMargin;
-	const innerSize = resultSize - transparentMargin; // 计算内部图像的大小
+	const innerSize = resultSize - transparentMargin * 2; // 计算内部图像的大小
 	try {
 		const imageBuffer = await fetchImage(url);
 		const mask = await sharp({
