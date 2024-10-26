ec44176997ae665832e914d2a5bd8733ccf354ea
xxx <a@b.c>
2024-10-26 23:25:28 +0800

    feature: test xx

--- src/runtime/node.js
+++ src/runtime/node.js
@@ -1,19 +1,3 @@
-// import 'tsx/esm';
+import 'tsx/esm';
 
-// await import('./node.ts');
-
-import express from 'express';
-
-const app = express();
-const port = 5000;
-app.get('/', (req, res) => {
-	res.status(200).json({ status: 'OK' });
-});
-
-app.listen(port, () => {
-	console.log('📝 Author: Razo');
-	console.log('🌍 GitHub Repository: https://github.com/iam2r/openai-tools');
-	console.log(`💖 Don't forget to star the repository if you like this project!`);
-	console.log();
-	console.log(`Server is running at http://localhost:${port}`);
-});
+await import('./express.ts');
