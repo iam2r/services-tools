a85d464697dbc814638f778b00bc9e3477f63aae
xxx <a@b.c>
2024-10-26 14:18:52 +0800

    feature: update pwa tools

--- public/libs/pwa-tools@0.0.1/pwa-tools.js
+++ public/libs/pwa-tools@0.0.1/pwa-tools.js
@@ -1,8 +1,39 @@
 (function (context) {
-	var replace2AbsolutePath = function (url) {
-		return url.replace(/^\//, new URL(`/`, window.location.origin).href);
+	const replace2AbsolutePath = (url) => url.replace(/^\//, new URL(`/`, window.location.origin).href);
+
+	const setupElement = (tagName, conditions, config = {}) => {
+		const selector = Object.entries(conditions)
+			.map(([key, value]) => `[${key}="${value}"]`)
+			.join('');
+
+		let tag = document.querySelector(`${tagName}${selector}`);
+		if (!tag) {
+			tag = document.createElement(tagName);
+			Object.entries({ ...conditions, ...config }).forEach(([key, value]) => {
+				tag.setAttribute(key, value);
+			});
+			document.head.appendChild(tag);
+		} else {
+			Object.entries(config).forEach(([key, value]) => {
+				tag.setAttribute(key, value);
+			});
+		}
+		return tag;
 	};
-	var PwaTools = {
+
+	const createAppleMeta = (name, icons = []) => {
+		setupElement('meta', { name: 'apple-mobile-web-app-capable' }, { content: 'yes' });
+		setupElement('meta', { name: 'apple-mobile-web-app-status-bar-style' }, { content: 'default' });
+
+		if (name) {
+			setupElement('meta', { name: 'apple-mobile-web-app-title' }, { content: name });
+		}
+		icons.forEach((icon) => {
+			setupElement('link', { rel: 'apple-touch-icon', sizes: icon.sizes }, { href: icon.src });
+		});
+	};
+
+	const PwaTools = {
 		setupManifest(manifestData) {
 			['scope', 'start_url'].forEach((key) => {
 				if (manifestData[key]) {
@@ -13,40 +44,33 @@
 				it.src = replace2AbsolutePath(it.src);
 				return it;
 			});
-			console.log(manifestData);
-			var link = document.createElement('link');
+			createAppleMeta(manifestData.name, manifestData.icons);
+			const link = document.createElement('link');
 			link.rel = 'manifest';
 			link.href = 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(manifestData));
 			document.head.appendChild(link);
 		},
 		initManifest(manifestUrl) {
 			fetch(manifestUrl)
-				.then(function (response) {
+				.then((response) => {
 					if (response.ok) {
 						return response.json();
 					} else {
 						throw new Error('Network response was not ok.');
 					}
 				})
-				.then(function (data) {
+				.then((data) => {
 					PwaTools.setupManifest(data);
 				})
-				.catch(function (error) {
+				.catch((error) => {
 					console.error('There was a problem with the fetch operation:', error);
 				});
 		},
 		registerThemeColor(light = '#f5f5f5', dark = '#1c1c1e') {
-			var lightMeta = document.createElement('meta');
-			lightMeta.setAttribute('name', 'theme-color');
-			lightMeta.setAttribute('media', '(prefers-color-scheme: light)');
-			lightMeta.setAttribute('content', light);
-			document.head.appendChild(lightMeta);
-			var darkMeta = document.createElement('meta');
-			darkMeta.setAttribute('name', 'theme-color');
-			darkMeta.setAttribute('media', '(prefers-color-scheme: dark)');
-			darkMeta.setAttribute('content', dark);
-			document.head.appendChild(darkMeta);
-			document.addEventListener('custom-event:updateThemeColor', function (event) {
+			const lightMeta = setupElement('meta', { name: 'theme-color', media: '(prefers-color-scheme: light)' }, { content: light });
+			const darkMeta = setupElement('meta', { name: 'theme-color', media: '(prefers-color-scheme: dark)' }, { content: dark });
+
+			document.addEventListener('custom-event:updateThemeColor', (event) => {
 				const theme = event.detail;
 				if (theme === 'light') {
 					lightMeta.setAttribute('content', light);
@@ -61,9 +85,7 @@
 			});
 		},
 		triggerThemeUpdate(theme) {
-			var event = new CustomEvent('custom-event:updateThemeColor', {
-				detail: theme,
-			});
+			const event = new CustomEvent('custom-event:updateThemeColor', { detail: theme });
 			document.dispatchEvent(event);
 		},
 	};
@@ -80,9 +102,7 @@
 
 	// ES Module export
 	if (typeof define === 'function' && define.amd) {
-		define([], function () {
-			return PwaTools;
-		});
+		define([], () => PwaTools);
 	} else if (typeof exports === 'object') {
 		exports.PwaTools = PwaTools;
 	}
