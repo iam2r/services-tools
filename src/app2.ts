3f2f7641c9769f36dac35d5e0f980719211ea2cd
xxx <a@b.c>
2024-10-26 23:41:58 +0800

    feature: xxx

--- src/app2.ts
+++ src/app2.ts
@@ -4,7 +4,6 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
 import { prettyJSON } from 'hono/pretty-json';
-import router from '@/routes/index.js';
 
 export const app = new Hono()
 	.use(logger())
@@ -13,5 +12,4 @@ export const app = new Hono()
 	.use('/static/*', serveStatic({ root: './', rewriteRequestPath: (path) => path.replace(/^\/static/, '/public') }))
 	.get('/ping', (c) => {
 		return c.json({ status: 'pong' });
-	})
-	.route('/', router);
+	});
