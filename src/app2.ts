14728efff79f74f4b2c66826056c2a50edc777fb
xxx <a@b.c>
2024-10-26 23:44:23 +0800

    feature:  test1

--- src/app2.ts
+++ src/app2.ts
@@ -1,5 +1,5 @@
 import '../config.js';
-import { serveStatic } from '@hono/node-server/serve-static';
+// import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
@@ -9,7 +9,7 @@ export const app = new Hono()
 	.use(logger())
 	.use(prettyJSON())
 	.use(cors())
-	.use('/static/*', serveStatic({ root: './', rewriteRequestPath: (path) => path.replace(/^\/static/, '/public') }))
+	// .use('/static/*', serveStatic({ root: './', rewriteRequestPath: (path) => path.replace(/^\/static/, '/public') }))
 	.get('/ping', (c) => {
 		return c.json({ status: 'pong' });
 	});
