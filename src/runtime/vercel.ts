13c6cad6c183126a2b96d6bd18a2676924f245d4
xxx <a@b.c>
2024-10-26 23:05:14 +0800

    feature: xxx

--- src/runtime/vercel.ts
+++ src/runtime/vercel.ts
@@ -1,8 +1,14 @@
-import { app } from '@/app.js';
+import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 
 export const config = {
 	runtime: 'edge',
 };
+
+const app = new Hono();
+
+app.get('/', (c) => {
+	return c.json({ message: 'Hello Hono!' });
+});
 
 export default handle(app);
