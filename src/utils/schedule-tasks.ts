7a48f6a93b1cb41bddc263dd56b36af57779398e
xxx <a@b.c>
2025-01-09 14:38:42 +0800

    feature: update schedule

--- src/utils/schedule-tasks.ts
+++ src/utils/schedule-tasks.ts
@@ -1,34 +1,32 @@
 import cron, { type ScheduledTask } from 'node-cron';
+
 export const scheduleTasks = () => {
 	if (!process.env.KEEP_ALIVE_URLS) return;
 	const urls = (process.env.KEEP_ALIVE_URLS || '').split(',');
 	if (urls.length) {
 		let job: ScheduledTask | null = null;
 
-		// 每小时重新生成随机时间并创建新的 cron 任务
 		const scheduleJob = () => {
 			if (job) {
 				job.stop(); // 停止上一次的 cron 任务
 			}
-
-			const now = new Date();
-			const currentMinute = now.getMinutes();
-			let randomMinute = Math.floor(Math.random() * 60);
-
-			// 如果生成的随机分钟数已经过去，则将其设置为下一分钟
-			if (randomMinute <= currentMinute) {
-				randomMinute = (currentMinute + 1) % 60;
-			}
-			const cronString = `${randomMinute} * * * *`; // 每小时的随机分钟执行
+			const cronString = `* * * * *`;
 			console.log(`${process.env.KEEP_ALIVE_URLS} is keepalive with ${cronString}!`);
 			job = cron.schedule(cronString, () => {
 				Promise.all(
 					urls.map((url) =>
-						fetch(url).catch(() => {
-							console.error(`Error sending keep-alive request for ${url}!`);
+						fetch(url).catch((error) => {
+							console.error(`Error sending keep-alive request for ${url}: ${error.message}`);
 						}),
 					),
-				).then(() => console.log('Keep-alive requests sent successfully'));
+				).then((responses) => {
+					responses.forEach((response, index) => {
+						if (!response?.ok) {
+							console.error(`Error: ${response?.status} for ${urls[index]}`);
+						}
+					});
+					console.log('Keep-alive requests sent successfully');
+				});
 			});
 
 			job.start();
